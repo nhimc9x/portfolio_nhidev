63286f7403f0f4927e3b9de938fe573d17e1ef47
nhimc9x <nhicomputer77@gmail.com>
2024-04-17 18:19:54 +0700

    Build Slice About

--- src/slices/About/About.jsx
+++ src/slices/About/About.jsx
@@ -1,5 +1,6 @@
 import { useSwiperSlide } from 'swiper/react'
 import clsx from 'clsx'
+import mySelfie from '~/assets/my_picture.jpg'
 import 'animate.css'
 
 function About() {
@@ -8,10 +9,54 @@ function About() {
     return clsx(className, swiperSlideActive ? animateIn : animateOut)
   }
   return (
-    <div className="w-full h-full px-[4%] flex flex-col-reverse lg:flex-row items-center justify-center">
-      <div className="bg-green-500 basis-3/5 w-full text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quis eum aut quaerat ad eaque dignissimos quidem ratione excepturi magnam natus, quos impedit necessitatibus commodi inventore enim repudiandae ullam magni?</div>
-      <div className="bg-pink-500 basis-2/5 w-full flex items-center">
-        <img className='h-full object-cover object-top' src="https://www.abusaid.me/_ipx/w_384,q_75/%2Fprofile.png?url=%2Fprofile.png&w=384&q=75" alt="" />
+    <div className="w-full h-full px-[4%] flex items-center justify-center">
+      <div className="flex w-full flex-col-reverse lg:flex-row">
+        <div className="basis-3/5 text-lg">
+          <div className={generateClassAnimate(
+            'font-semibold uppercase text-ct-sub-active-color mb-2 animate__animated',
+            'animate__bounceInLeft',
+            'animate__bounceOutUp'
+          )}>
+            Who I am?
+          </div>
+          <div className={generateClassAnimate(
+            'text-ct-text-color leading-8 animate__animated',
+            'animate__bounceIn animation-delay-600',
+            'animate__bounceOutUp'
+          )}>
+            <div className="">
+              My name is
+              <span className='text-ct-active-color'> Vu Van Nhi</span>
+              , a third-year student at Vietnam Vocational College of Commerce and Industry.
+            </div>
+            <div className="">
+              Currently, I am delving into
+              <span className='text-ct-active-color'> Frontend development </span>
+              and aspire to become a
+              <span className='text-ct-active-color'> Full Stack Developer </span>
+              in the future.
+            </div>
+            <div className="">
+              Apart from coding, some other activities that I love to do include playing football, exploring technology, and watching movies.
+            </div>
+            <div className="">
+              I am available for any job opportunity that aligns with my skills and aspirations.
+            </div>
+          </div>
+        </div>
+        <div className="basis-2/5 flex justify-center items-center">
+          <div className={generateClassAnimate(
+            'h-full min-h-[380px] w-[300px] rounded p-1.5 overflow-hidden flex justify-center items-center relative animate__animated',
+            'animate__fadeInRight animation-delay-500',
+            'animate__bounceOutUp'
+          )}>
+            <div className="absolute h-[160%] w-[500px] bg-gradient-to-t animate-spin from-cyan-500 to-pink-500"></div>
+            <img
+              className="h-full z-10 w-full rounded-sm object-cover object-top"
+              src={mySelfie}
+              alt="My Avatar" />
+          </div>
+        </div>
       </div>
     </div>
   )
