1e81128582edf840a272cbb86b3591ef8b50999f
nhimc9x <nhicomputer77@gmail.com>
2024-05-04 23:22:13 +0700

    Custom Cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import MainSlider from '~/slider/MainSlider/MainSlider'
+import AnimatedCursor from 'react-animated-cursor'
 
 function App() {
 
   return (
     <>
+      <AnimatedCursor
+        innerSize={12}
+        outerSize={34}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: '#ffffff70'
+        }}
+        outerStyle={{
+          border: '1px solid #eeeeee80'
+        }}
+      />
       <MainSlider />
     </>
   )
