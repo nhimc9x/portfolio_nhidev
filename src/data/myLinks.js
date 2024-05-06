0a18e6f3b16de05edd0e878ae5f728f569317c02
nhimc9x <nhicomputer77@gmail.com>
2024-05-06 10:54:50 +0700

    update link

--- src/data/myLinks.js
+++ src/data/myLinks.js
@@ -5,5 +5,5 @@ export default {
   linked: 'https://www.linkedin.com/in/nh%E1%BB%8B-v%C5%A9-v%C4%83n-499b74307',
   discord: 'https://discord.com/channels/687817231572271206/687817232054747205',
   zalo: '',
-  downloadCV: 'https://www.topcv.vn/download-cv?cv_id=V1QFBF9TCwFSVllRBgwEVVEFBQ1QUVoHVgYDBQ7cbf'
+  downloadCV: 'https://www.topcv.vn/download-cv?cv_id=BQYJBQEFVFBVBVFTAw4GVQ8DAVtVVVpdBFdQWQ53bc'
 }
